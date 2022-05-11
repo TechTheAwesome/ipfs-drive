4a170c31268b87d6c7f65e8ffa25f7f8ee9e5ce7
TechTheAwesome <techtheawesome01@gmail.com>
2022-05-12 00:27:30 +0900

    ADD default setting

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,10 @@
+import { TSetting } from "./hooks/useSetting";
+
 export const IPFS_STORE = 'ipfs-drive'
 export const IPFS_STORE_PATH = `/${IPFS_STORE}`;
 export const IPFS_CONFIG = 'store.config';
 export const IPFS_CONFIG_PATH = `${IPFS_STORE_PATH}/${IPFS_CONFIG}`;
+
+export const DEFAULT_SETTING: TSetting = {
+  drive: []
+}
